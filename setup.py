59b277d259aa9c42af95e3a5c82fcc37c44929b1
amsmiles <76047929+amsmiles@users.noreply.github.com>
2021-02-20 18:04:08 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ extras_require = {
     ]
 }
 
-setup(name='discordvoice.py',
+setup(name='discord.py',
       author='Rapptz',
       url='https://github.com/Rapptz/discord.py',
       project_urls={
